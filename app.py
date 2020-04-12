3cc9dfa8049774eec389b32e401a044038d3632c
gijs <gijs@m3consultancy.nl>
2020-04-12 10:20:47 +0200

    daily update

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ navbar = dbc.Container(
                     ])
 
 ##dashboard page
-page_1_layout = html.Div([navbar, 
+page_1_layout = html.Div([navbar,
                  dbc.Container([
                     dbc.Jumbotron([
                         dcc.Markdown('''
@@ -75,9 +75,9 @@ page_1_layout = html.Div([navbar,
                             Like most, we follow the news on Corona daily. We are bombarded with numbers on deaths, hospitalisations and projections on the availability of IC beds. However, to us, the torrent of daily numbers lack context. Without this context it is hard to make sense of it all. Specifically, we fail to find the answer to our two main questions:
 
                             1. Are we slowing down the spread?
-                            
+
                             2. Will we have enough IC beds?
-                           
+
                             Our dashboard below tries to provide answers to these two questions based on the latest data available. **This dashboard is updated every day at midnight**.
                             ''')],
                         className="m3-jumbo"),
@@ -99,9 +99,9 @@ This is not an easy question to answer, as it involves the future. To answer it,
 To forecast demand, we have created our own, simplified, forecasting model based on the [SEIR model](https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology#The_SEIR_model).  A key variable in this model is the reproduction rate (R) :  the number of people a person infected with COVID-19 infects.  The bulk of the measures taken in The Netherlands are aimed at lowering this reproduction rate. Lowering R means less people get infected at the same time, which means less people need IC care simultaneously: flattening the curve.
 
 To gauge the effectiveness of measures taken so far and to project their result on IC demand we follow the approach described below.
-Based on the available data, our model estimates R for two periods: 
-* (i) the initial period when measures were not yet taken and 
-* (ii) the suppression period, after the implementation of our “intelligent lockdown” . 
+Based on the available data, our model estimates R for two periods:
+* (i) the initial period when measures were not yet taken and
+* (ii) the suppression period, after the implementation of our “intelligent lockdown” .
                         '''),
 
                     dbc.Row([
@@ -127,7 +127,7 @@ Based on the available data, our model estimates R for two periods:
                     dcc.Graph(id = 'outlook_figure', className="m3-graph"),
                     dcc.Markdown('''
                     Time lag plays an important role in projecting demand for IC beds. The effects of the NL measures did not have an immediate impact on hospitalisation and IC rates. It takes xx days on average from initial infection to needing IC care and yy days after that before the IC bed is released.  Because of this, the current numbers still include patients which were infected before the NL measures were implemented. As a result, our estimates of R and corresponding IC demand are still changing daily as the share of patients infected before the NL measures declines.
-                    
+
                     To see the impact of different values of R for yourself, you can change the slider next to the graph above and it will show you the effects on IC demand. More interactive results and background on our model can be found on the [background page](/background).
                     '''),
                     html.P(["Number of deaths per country. Source: John Hopkins University. lastly retrieved per: ", str(date.today().strftime("%d/%m/%Y"))+" 00:00 CET"], className="m3-footnote"),
@@ -137,12 +137,12 @@ Based on the available data, our model estimates R for two periods:
 ## background page
 page_2_layout = html.Div([
                     navbar,
-                    dbc.Container([                        
+                    dbc.Container([
                         dcc.Markdown('''### A model to forecast the COVID-19 outbreak
 Modelling the COVID-19 outbreak is a complex endeavour: very little is known about key factors such as the infectious period, incubation time, mortality rate and reproduction rate. Models by any party therefore carry a high amount of uncertainty. This does not mean they should not be made though, as they do provide insight in what direction the outbreak is progressing and are key to supporting decision-makers in their next steps.
 
-To keep ourselves up to date on the latest forecast, we have created our own model to forecast the development of the COVID-19 outbreak. It is a so-called compartmental model, a common approach to modelling epidemics. You can find a good description of a simple compartmental model  [in this video](https://www.youtube.com/watch?v=Qrp40ck3WpI) 
-If you are curious as to how we implemented the compartmental model and what parameters we used, have a look at the source code and further descriptions on our  [Github.](https://github.com/ErwinM/m3_covid19) 
+To keep ourselves up to date on the latest forecast, we have created our own model to forecast the development of the COVID-19 outbreak. It is a so-called compartmental model, a common approach to modelling epidemics. You can find a good description of a simple compartmental model  [in this video](https://www.youtube.com/watch?v=Qrp40ck3WpI)
+If you are curious as to how we implemented the compartmental model and what parameters we used, have a look at the source code and further descriptions on our  [Github.](https://github.com/ErwinM/m3_covid19)
 
 To give you an idea of how the model works, Figure 5 outlines the various outcomes of the model and lets you see the impact of changing some of the key input parameters.
                             '''),
@@ -158,12 +158,12 @@ To give you an idea of how the model works, Figure 5 outlines the various outcom
                                     {'label': 'Infectious', 'value': 'Infectious'},
                                     {'label': 'Recovered', 'value': 'Recovered'},
                                     {'label': 'Susceptible', 'value': 'Susceptible'}
-                                   
+
                                 ],
                                 value=['Infectious','Patients in hospital', 'Patients on IC'],
                                 multi=True
-                            ),  
-                        
+                            ),
+
                         html.P(["Days to forecast"], style = {"marginTop": "10px"}),
                             dcc.Slider(
                                 id = 'days_slider',
@@ -215,8 +215,8 @@ To give you an idea of how the model works, Figure 5 outlines the various outcom
                                     10: '10',
                                     20: '20',
                                     30: '30',
-                                    40: '40'}),                            
-                        ], 
+                                    40: '40'}),
+                        ],
                     style = {"margin-top":"100px",
                              "margin-bottom":"200px",
                              "width": "900px"})])
@@ -230,13 +230,13 @@ app.layout = html.Div(
 @app.callback(
     Output('outlook_figure', 'figure'),
     [Input('I1_slider', 'value')])
-def update_figure1(R):    
+def update_figure1(R):
     #TO DO: integrate most of this into forecaster class
     # get fitted model
     solution_outlook =  forecaster.forecasts["outlook"]
     solution_prev = forecaster.forecasts["previous_forecast"]
     solution_3d = forecaster.forecasts["3d_ago_forecast"]
-    solution_target = forecaster.SEIR_solution(intervention = [(30,factors[0]),(300,R/2.2)])    
+    solution_target = forecaster.SEIR_solution(intervention = [(30,factors[0]),(300,R/2.2)])
 
     # create data sets for figures with outlook IC utilization and target IC utilization
     y_ic_outlook = solution_outlook["I_ic"] + solution_outlook["I_fatal"] * 0.5
@@ -267,7 +267,7 @@ def update_figure1(R):
             )
     outlook_fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightGrey')
     outlook_fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='LightGrey')
-    return outlook_fig 
+    return outlook_fig
 
 # interaction for figure 5 with sliders
 @app.callback(
@@ -278,23 +278,23 @@ def update_figure1(R):
      Input('Inc_slider', 'value'),
      Input('IC_slider', 'value'),
      ])
-def update_figure1(measures, R, days, inc, IC):    
+def update_figure1(measures, R, days, inc, IC):
     #TO DO: integrate most of this into forecaster class
 
     # get fitted model
     solution_outlook =  forecaster.SEIR_solution(intervention = [(30,factors[0]),(len(forecaster.hospitals),R/2.2), (10000,R/2.2)],
                                                  days = days, t_inc = inc, t_ic = IC)
     solution_outlook["IC_total"] = solution_outlook["I_ic"] + solution_outlook["I_fatal"] * 0.5
-    solution_outlook["R_total"] = solution_outlook["R_mild"] + solution_outlook["R_hosp"]  + solution_outlook["R_ic"]  
-    solution_outlook["Hosp_tot"] = solution_outlook["I_ic"] + solution_outlook["I_hosp"] + solution_outlook["I_fatal"] 
+    solution_outlook["R_total"] = solution_outlook["R_mild"] + solution_outlook["R_hosp"]  + solution_outlook["R_ic"]
+    solution_outlook["Hosp_tot"] = solution_outlook["I_ic"] + solution_outlook["I_hosp"] + solution_outlook["I_fatal"]
     x_outlook = pd.date_range(start='16/2/2020', periods=len(solution_outlook))
 
     # create figure
     outlook_fig = go.Figure()
 
     for measure in measures:
-        outlook_fig.add_trace(go.Scatter(y=solution_outlook[mapping[measure]], 
-                                         x= x_outlook, 
+        outlook_fig.add_trace(go.Scatter(y=solution_outlook[mapping[measure]],
+                                         x= x_outlook,
                                          name = measure,
                                          hovertemplate = '%{y:.0f}'))
 
@@ -325,11 +325,11 @@ def display_page(pathname):
     else:
         return page_1_layout
 
-# serve app
+# serve app 
 is_prod = os.environ.get('IS_HEROKU', None)
 
 if __name__ == '__main__':
     if is_prod:
         app.run_server(debug=False)
     else:
-        app.run_server(debug=False)
+        app.run_server(debug=True)
